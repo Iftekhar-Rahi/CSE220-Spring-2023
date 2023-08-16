95ade5ca4e8fb6488abec7296008d2a86d0b6e5c
Rahi <iftekharhossainrahi@gmail.com>
2023-08-16 19:56:39 +0600

    Rahi

---
+++ My practice Files/3. Linked List/Practice before make-up Mid.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+head=Node(11)
+
+a=Node(3)
+
+b=Node(23)
+
+c=Node(7)
+
+d=Node(4)
+
+head.next=a
+a.next=b
+b.next=c
+c.next=d
+def print_ll(head):
+    tail=head
+    while tail!=None:
+        print(tail.value,end="-->")
+        tail=tail.next
+print_ll(head)
--- My practice Files/8. Hashing/1. Constructor.py
+++ My practice Files/8. Hashing/1. Constructor.py
@@ -1,7 +1,14 @@
-class Hashing:
+class Hash_Table:
     def __init__(self,size=7):
         self.data_map=[None]*size
     def hash_function(self,key):
         hash_value=0
         for letter in key:
-            hash_value=
+            hash_value+=ord(letter)
+        hash_value=hash_value%len(self.data_map)
+        return hash_value
+    def print_hash_table(self):
+        for i,j in enumerate(self.data_map):
+            print(i,":", j)
+my_hash_table=Hash_Table()
+my_hash_table.print_hash_table()
